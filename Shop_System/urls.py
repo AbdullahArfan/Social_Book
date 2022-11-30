561cb201cbfa76b9e4e8137c6d45044cc34bfb03
Abdullah <abdullaharfan106@gmail.com>
2022-11-30 15:47:53 +0600

    second commit

--- Shop_System/urls.py
+++ Shop_System/urls.py
@@ -4,7 +4,6 @@ from Shop_System import views
 app_name = 'Shop_System'
 
 urlpatterns = [
-    # path('', views.Home.as_view(), name='home'),
     path('market/', views.MarketHome.as_view(), name='market'),
     path('product/<pk>/', views.ProductDetail.as_view(), name='product_detail'),
 ]
